603b01fbc35816c0de7bede37f09e1b29b15e926
softcomet <realseniordev210@gmail.com>
2018-03-20 20:42:59 +0100

    enable default locale

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,6 @@ module Appenem
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-    # config.i18n.default_locale = 'pt-BR'
+    config.i18n.default_locale = 'pt-BR'
   end
 end
