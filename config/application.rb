55b9aa4429689bcff4ee82d796bdc0353e43b712
softcomet <realseniordev210@gmail.com>
2018-03-20 18:23:20 +0100

    modify app setting

--- config/application.rb
+++ config/application.rb
@@ -25,7 +25,7 @@ module Appenem
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
-    # config.api_only = true
+    config.api_only = true
     # config.i18n.default_locale = 'pt-BR'
   end
 end
